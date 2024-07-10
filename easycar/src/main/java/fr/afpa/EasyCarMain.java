e1294eb218286d5c0d25a19f8cb35075f9b61944
Your NameRudolph <attissorudolph@gmail.com>
2024-07-10 08:45:24 +0200

    tentative d'ajout des logs mais exercice pas adapté

--- easycar/src/main/java/fr/afpa/EasyCarMain.java
+++ easycar/src/main/java/fr/afpa/EasyCarMain.java
@@ -2,12 +2,17 @@ package fr.afpa;
 
 import java.time.LocalDate;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class EasyCarMain 
 {
+    	// Creating a logger
+	private static Logger logger = LogManager.getLogger();
     public static void main( String[] args )
     {
         Bike bike = new Bike("decat", "riverside", "gris", LocalDate.parse("2023-06-05"), 25.5, 'M');
@@ -23,7 +28,12 @@ public class EasyCarMain
         System.out.println(reservation1.totalPrice());
         System.out.println(customer1.totalMoneySpend());
         
-        
+        logger.debug("It is a debug logger.");
+		logger.error("It is an error logger.");
+		logger.fatal("It is a fatal logger.");
+		logger.info("It is a info logger.");
+		logger.trace("It is a trace logger.");
+		logger.warn("It is a warn logger.");
 
     }
 }
